491159293f54eca84790e759069d6a8041298752
UrsulKhan21 <abdurursulkhan@gmail.com>
2026-01-30 19:47:36 +0530

    remove llama_index

--- data_loader.py
+++ data_loader.py
@@ -1,6 +1,3 @@
-from llama_index.readers.file import PDFReader
-from llama_index.core.node_parser import SentenceSplitter
-# ---------------- FUNCTIONS ---------------- #
 import torch
 from sentence_transformers import SentenceTransformer
 
@@ -14,13 +11,6 @@ embedder = SentenceTransformer(
 embedder.encode(["warmup"], show_progress_bar=False)
 
 
-def load_and_chunk_pdf(path: str) -> list[str]:
-    docs = PDFReader().load_data(file=path)
-    texts = [d.text for d in docs if getattr(d, "text", None)]
-    chunks = []
-    for text in texts:
-        chunks.extend(splitter.split_text(text))
-    return chunks
 
 
 def embed_texts(texts: list[str]) -> list[list[float]]:
